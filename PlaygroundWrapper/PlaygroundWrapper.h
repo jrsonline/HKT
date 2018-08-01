44d73d547268119b4ba34c1368f78e01d138b345
strictlyswift <lilyonline@me.com>
2018-07-31 23:13:33 -0400

    Fix to enable Playground to run

---
+++ PlaygroundWrapper/PlaygroundWrapper.h
@@ -0,0 +1,19 @@
+//
+//  PlaygroundWrapper.h
+//  PlaygroundWrapper
+//
+//  Created by RedPanda on 7/31/18.
+//  Copyright © 2018 @simplyswift. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlaygroundWrapper.
+FOUNDATION_EXPORT double PlaygroundWrapperVersionNumber;
+
+//! Project version string for PlaygroundWrapper.
+FOUNDATION_EXPORT const unsigned char PlaygroundWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlaygroundWrapper/PublicHeader.h>
+
+
